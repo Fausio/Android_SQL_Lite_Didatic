51cfbfbd8eefdca53ed5827d260d5e9194a41709
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-06-01 09:08:31 +0200

    household model

---
+++ app/src/main/java/com/ustm/domains/household.java
@@ -0,0 +1,35 @@
+package com.ustm.domains;
+
+import java.math.BigInteger;
+
+public class household {
+
+    private Integer id;
+    private  String name;
+    private  String address;
+    private BigInteger cell;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public BigInteger getCell() {
+        return cell;
+    }
+
+    public void setCell(BigInteger cell) {
+        this.cell = cell;
+    }
+}
